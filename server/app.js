a2dc65d090abf4b846136c9a486111d1e982ecc1
codewithmunmun <munmunkum8210@gmail.com>
2025-08-02 12:34:22 +0530

    route: Added user auth route and controllers

--- server/app.js
+++ server/app.js
@@ -1,6 +1,9 @@
 const morgan = require("morgan");
 const express = require("express");
 const cookieParser = require("cookie-parser");
+const session = require('express-session');
+
+const userRouter = require('./routes/userRoutes')
 
 const app = express();
 
@@ -8,5 +11,20 @@ if (process.env.NODE_ENV === "development") app.use(morgan("dev"));
 
 app.use(express.json());
 app.use(cookieParser());
+
+app.use(
+    session({
+        secret: process.env.SESSION_SECRET,
+        resave: false,
+        saveUninitialized: false,
+        cookie: {
+            httpOnly: true,
+            maxAge: 1000 * 60 * 60 * 24, // 1 day
+        },
+    })
+);
+
+// Routers
+app.use("/api/v1/users", userRouter);
 
 module.exports = app
